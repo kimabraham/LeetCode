e071eca10b6b93635e4cf94e7b5093fec83a4718
kimabraham <kimkr88@icloud.com>
2024-06-24 09:35:14 +0900

    Time: 294 ms (95.57%), Space: 148.9 MB (53.19%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.dart
+++ 0020-valid-parentheses/0020-valid-parentheses.dart
@@ -6,23 +6,16 @@ final Map<String, String> map = {
 
 class Solution {
   bool isValid(String s) {
-    final List<String> stack = [];
-    final brackets = s.split('');
-
-    for(var bracket in brackets) {
-        if(map.containsValue(bracket)) {
-            stack.add(bracket);
-            continue;
-        }
-        if(stack.isEmpty) {
-            stack.add(bracket);
-        } else {
-            if(map[bracket] == stack.last) {
-                stack.removeLast();
-            }else{
-                stack.add(bracket);
-            }
-        }
+        final List<String> stack = [];
+    
+    for (var bracket in s.split('')) {
+      if (map.containsValue(bracket)) {
+        stack.add(bracket);
+      } else if (stack.isNotEmpty && map[bracket] == stack.last) {
+        stack.removeLast();
+      } else {
+        return false;
+      }
     }
 
     return stack.isEmpty;
