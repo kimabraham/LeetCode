72aa5d51875956e58db9cc97e003802c242d7583
kimabraham <kimkr88@icloud.com>
2024-06-24 10:06:45 +0900

    Time: 342 ms (29.02%), Space: 149.3 MB (51.72%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.dart
+++ 0014-longest-common-prefix/0014-longest-common-prefix.dart
@@ -1,9 +1,9 @@
 class Solution {
   String longestCommonPrefix(List<String> strs) {
-    strs.sort((a, b) => a.length.compareTo(b.length));
+    // strs.sort((a, b) => a.length.compareTo(b.length));
     
     String prefix = strs[0];
-    
+
     for (int i = 1; i < strs.length; i++) {
       while (!strs[i].startsWith(prefix)) {
         prefix = prefix.substring(0, prefix.length - 1);
